e149f404d924673f7a996a9de20f3ea7e23ce9d4
xStranged <118495317+xStranged1@users.noreply.github.com>
2023-11-07 21:21:44 -0300

    Update sectionMe.jsx

--- src/components/sectionMe.jsx
+++ src/components/sectionMe.jsx
@@ -13,7 +13,7 @@ export default function SectionMe() {
                 <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-4" style={{textAlign: 'left'}}>Federico Valle</h1>
                 <p className="lead" style={{textAlign: 'left'}}>Soy un estudiante de 3er año de Ingeniería en Sistemas de Información en la UTN FRLP.</p>
                 <p className="lead" style={{textAlign: 'left'}}>Me gusta analizar problemas reales, diseñar soluciones, desarrollar sitios web, tengo facilidad para aprender nuevas tecnologías, tambien me gusta la ciencia, la fisica, entre otras cosas.</p>
-                <p className="lead" style={{textAlign: 'left'}}>Aprendí a programar en 3er año de secundaria, me encanto y decidí esto es a lo que me queria dedicar.</p>
+                <p className="lead" style={{textAlign: 'left'}}>Aprendí a programar a los 15 años en la escuela secundaria, desde entonces decidí dedicarme al campo de sistemas.</p>
                 
             </div>
             </div>
